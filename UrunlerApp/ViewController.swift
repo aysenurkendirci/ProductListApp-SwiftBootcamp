6e3b002de69cc3c2d5f24940d08daad4f4d31982
aysenurkendirci <aysenurkendirciss@gmail.com>
2025-07-12 16:15:38 +0300

    Initial Commit

---
+++ UrunlerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UrunlerApp
+//
+//  Created by Kübra Kendirci on 12.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
